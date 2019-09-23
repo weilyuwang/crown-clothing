93a740d3dfdc436c3dd7c408e161738b9e4268e1
Weilyu Wang <weilyuwang@gmail.com>
2019-09-23 19:13:28 -0400

    adding header components, logo, and links to home, shop, contact page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
+//want <Header> component to be present on every page, so put it out of the <Switch><Route>
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
