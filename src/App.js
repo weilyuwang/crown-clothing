e9c46ebadd8b93e46fefedeefdb1885fb0b345f1
Weilyu Wang <weilyuwang@gmail.com>
2019-09-24 16:09:08 -0400

    implemented firebase utils, included ability to store autheticated users into our firebase database

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+          console.log(this.state);
         });
       }
       //if userAuth == null, set our state to null as well:
