b0db8441e38ca9eb6c68da110c47edf53992fbfd
weilyuwang <weilyuwang@gmail.com>
2019-12-24 13:08:56 -0500

    remove unused imports

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CartIcon from "../cart-icon/cart-icon.component";
